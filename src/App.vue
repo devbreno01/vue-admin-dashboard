8f8f3c5347935013b047274fc7db3ebe2d195fc8
Breno Guimarães <devbreno4@gmail.com>
2026-06-23 12:42:11 -0300

    fet: sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,67 @@
-<script setup></script>
 
+
+<script setup>
+  import { ref } from 'vue';
+  import Drawer from 'primevue/drawer';
+  import Button from 'primevue/button';
+
+
+  const visible = ref(true)
+</script>
 <template>
-  <nav>
-      <router-link to="/">
-         <a href="#">Home</a>
-      </router-link>
-  </nav>
+    <Drawer v-model:visible="visible" header="BrenoAdmin"  class="bg-sidebar!">
+      <nav class="" >
+          <ul class="sidebar-ul">
+              <li class="mb-4 h-12 flex items-center">
+                <i class="pi pi-home ml-3" style="font-size: 1rem;"></i>
+                  <router-link to="/">
+                    <a href="#" class="ml-2 text-lg">Dashboard</a>
+                  </router-link>
+                
+              </li>
+               <li class="mb-4 h-12 flex items-center">
+                <i class="pi pi-box ml-3" style="font-size: 1rem;"></i>
+                  <router-link to="/">
+                    <a href="#" class="ml-2 text-lg">Products</a>
+                  </router-link>
+                
+              </li>
+              <li class="mb-4 h-12 flex items-center">
+                <i class="pi pi-folder ml-3" style="font-size: 1rem;"></i>
+                  <router-link to="/">
+                    <a href="#" class="ml-2 text-lg">Categories</a>
+                  </router-link>
+                
+              </li>
+              <li class="mb-4 h-12 flex items-center">
+                <i class="pi pi-shopping-bag ml-3" style="font-size: 1rem;"></i>
+                  <router-link to="/">
+                    <a href="#" class="ml-2 text-lg">Orders</a>
+                  </router-link>
+                
+              </li>
+              <li class="mb-4 h-12 flex items-center">
+                <i class="pi pi-user ml-3" style="font-size: 1rem;"></i>
+                  <router-link to="/">
+                    <a href="#" class="ml-2 text-lg">Users</a>
+                  </router-link>
+                
+              </li>
+               <li class="mb-4 h-12 flex items-center">
+                <i class="pi pi-cog ml-3" style="font-size: 1rem;"></i>
+                  <router-link to="/">
+                    <a href="#" class="ml-2 text-lg">Settings</a>
+                  </router-link>
+                
+              </li>
+          </ul>
+      </nav>
+    </Drawer>
 
-  <main>
-    <router-view />
-  </main>
+   <Button icon="pi pi-arrow-right" @click="visible = true" />
+    <main>
+      <p>tasdasdasd</p>
+    </main>
 </template>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'; 
+import 'primeicons/primeicons.css'; 
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import '../src/style.css'; 
 
+const app = createApp(App);
 
-createApp(App)
-    .use(router)
-    .use(PrimeVue, {
-        theme: {
-            preset: Aura
-        }
-    })
-    .mount('#app')
+app.use(router)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+
+app.mount('#app')
